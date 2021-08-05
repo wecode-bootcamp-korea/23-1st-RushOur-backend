c917044ce1bd80025a1f693eefdd6a8d55a736e6
jake <tkddnd82@gmail.com>
2021-08-05 18:30:02 +0900

    feature/user4

--- carts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from carts.views import CartView,CartDetailView
-
-urlpatterns = [ 
-	path ('/cart', CartView.as_view()),
-	path ('/cart/<int:cart_id>', CartDetailView.as_view())
-] 
