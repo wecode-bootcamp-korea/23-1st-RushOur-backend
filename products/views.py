8b59f5f5bfbe91fceb698e1b6691746b48d3c972
최민 <cm9542@gmail.com>
2021-08-05 17:50:08 +0900

    Add: 메뉴 카테고리 바 기능구현

--- products/views.py
+++ products/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views    import View
+from django.http     import JsonResponse
+
+from products.models import Category 
+
+class NavigatorView(View):
+    def get(self, request):
+        categories = Category.objects.all()
+        category_lst = []
+        for category in categories:
+            sub_categories = category.subcategory_set.all()
+            sub_category_lst = []
+            for sub_category in sub_categories:
+                sub_category_lst.append({
+                    'sub_category_id'   : sub_category.id,
+                    'name' : sub_category.name
+                })
+            category_lst.append({
+                'category_id'  : category.id,
+                'name': category.name,
+                'sub_categories' : sub_category_lst
+            })
+        return JsonResponse({"categories":category_lst}, status=200)
