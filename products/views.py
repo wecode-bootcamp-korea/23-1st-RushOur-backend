33cfd5b4300602b2b8fe731adab25d0507adc9b1
최민 <cm9542@gmail.com>
2021-08-06 21:08:01 +0900

    [23기 최민] Add: 상품디테일페이지 기능 구현

--- products/views.py
+++ products/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views    import View
+from django.http     import JsonResponse
+
+from products.models import Category, ProductDetailImage, SubCategory, Product, Tag
+
+class ProductDetailView(View):
+    def get(self, request, product_id):
+        if not Product.objects.filter(id=product_id).exists():
+            return JsonResponse({"MESSAGE":"INVALID_ID"}, status=404)
+
+        categories    = Category.objects.all()
+        subcategories = SubCategory.objects.all()
+        product       = Product.objects.get(id=product_id)
+        detail_page = {
+            'categories'   : [category.name for category in categories],
+            'subcategories': [subcategory.name for subcategory in subcategories],
+            'id'           : product.id,
+            'name'         : product.name,
+            'thumbnail'    : product.thumbnail_image_url,
+            'options'      : [{
+                'size'  : option.size,
+                'price' : option.price
+                                } for option in product.option_set.all()],
+            'tags'         : [tag.name for tag in product.tags.all()],
+            'detail_img'   : ProductDetailImage.objects.get(product=product).image_url
+        }
+        return JsonResponse({"detail_page":detail_page}, status=200)
+
