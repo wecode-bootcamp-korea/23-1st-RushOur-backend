ed687866a63bdb24f00d09e6497c8613f7ba7bc3
jake <tkddnd82@gmail.com>
2021-08-06 14:15:38 +0900

    feature/user6

--- users/models.py
+++ users/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class User(models.Model):
     username         = models.CharField(max_length = 50,unique=True)
-    password         = models.CharField(max_length = 200,unique=True)
+    password         = models.CharField(max_length = 200)
     name             = models.CharField(max_length = 50)
     nickname         = models.CharField(max_length = 50, null=True)
     email            = models.CharField(max_length = 320,unique=True)
-    phone_number     = models.CharField(max_length = 50)
+    phone_number     = models.CharField(max_length = 50,unique=True)
     address          = models.CharField(max_length = 300, null=True)
     created_at       = models.DateTimeField(auto_now_add = True)
 
