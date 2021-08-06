4e7dfa00316f8e4041e71f0293efe642a10bc1b2
최민 <cm9542@gmail.com>
2021-08-06 17:56:30 +0900

    [23기 최민] Add: 상품리스트 기능 추가, 필터링 기능 구현

--- products/views.py
+++ products/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views    import View
+from django.http     import JsonResponse
+
+from products.models  import Category, SubCategory, Product, Tag
+from django.db.models import Min, Q
+
+class ProductsView(View):
+    def get(self, request):
+        category_id    = request.GET.get('category') 
+        subcategory_id = request.GET.get('subcategory')
+        tags           = request.GET.getlist('tag')
+        sort           = request.GET.get('sort')
+
+        product_filter = Q()
+        
+        if category_id:
+            product_filter.add(Q(sub_category__category=category_id), Q.AND)
+
+        if subcategory_id:
+            product_filter.add(Q(sub_category=subcategory_id), Q.AND)
+             
+        products = Product.objects.filter(product_filter)
+        
+        if tags:
+            for tag in tags:
+                products = products.filter(tags__name=tag)
+
+        if sort:
+            products = products.annotate(price=Min('option__price')).order_by(sort)
+
+        products_lst = [{
+                'id'        : product.id,
+                'name'      : product.name,
+                'price'     : product.option_set.all()[0].price,
+                'thumbnail' : product.thumbnail_image_url,
+                'tags'      : [tag.name for tag in product.tags.all()]
+            }for product in products]
+
+        return JsonResponse({'products':products_lst}, status=200)
