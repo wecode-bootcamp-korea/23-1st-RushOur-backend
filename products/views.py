09605ffb7d5e651bf79560ec20d62785813cea91
최민 <cm9542@gmail.com>
2021-08-08 13:43:43 +0900

    [23기 최민] Modify: Conflict 해결, 수정사항 반영

--- products/views.py
+++ products/views.py
@@ -21,19 +21,19 @@ class ProductsView(View):
         if subcategory_id:
             product_filter.add(Q(sub_category=subcategory_id), Q.AND)
              
-        products = Product.objects.filter(product_filter)
+        products = Product.objects.filter(product_filter).annotate(price=Min('option__price')).order_by('name')
         
         if tags:
             for tag in tags:
                 products = products.filter(tags__name=tag)
 
         if sort:
-            products = products.annotate(price=Min('option__price')).order_by(sort)
+            products = products.order_by(sort)
 
         products_lst = [{
                 'id'        : product.id,
                 'name'      : product.name,
-                'price'     : product.option_set.all()[0].price,
+                'price'     : product.price,
                 'thumbnail' : product.thumbnail_image_url,
                 'tags'      : [tag.name for tag in product.tags.all()]
             }for product in products]
