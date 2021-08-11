33d39e07de3b04ee63531e6374ced1f275c0cfe1
jake <tkddnd82@gmail.com>
2021-08-11 12:57:04 +0900

    Refactor: 리뷰 반영 -전체적으로 컨벤션 맞춰서 수정

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,14 +1,14 @@
 import json
-from my_settings import SECRET_KEY
+from my_settings            import SECRET_KEY
 
-from wishlists.models import Wishlist
+from wishlists.models       import Wishlist
 
 from django.http            import JsonResponse
 from django.views           import View
 from django.db.models       import Q, Min
 from products.models        import (Category,SubCategory,Product,Option,ProductDetailImage,Tag,)
-from users.utils             import login_required 
-from users.models            import User
+from users.utils            import login_required 
+from users.models           import User
 
 
 class WishlistView(View):
@@ -19,14 +19,13 @@ class WishlistView(View):
             like_list      = user.wishlist_set.all().annotate(price=Min("product__option__price"))
             
             like_items = [{
-            'product_id'   : like.product.id,
-            'name'         : like.product.name,
-            'image'        : like.product.thumbnail_image_url,
-            'price'        : like.price
-            }for like in like_list]
+                'product_id'   : like.product.id,
+                'name'         : like.product.name,
+                'image'        : like.product.thumbnail_image_url,
+                'price'        : like.price} for like in like_list]
+            return JsonResponse({'Like_Items' : like_items}, status = 200)
         except KeyError:
             return JsonResponse({'Message' : 'KEY_ERROR'}, status = 400)
-        return JsonResponse({'Like_Items' : like_items}, status = 200)
     
     @login_required
     def post(self,request):
@@ -38,16 +37,11 @@ class WishlistView(View):
             if Wishlist.objects.filter(Q(product_id=product_id) & Q(user_id=user_id)).exists():
                Wishlist.objects.filter(product_id=product_id).delete()
                return JsonResponse({'Message' : 'SUCCESS_DELETE'}, status = 200)
-        
-
             else:
                 Wishlist.objects.create(
                 product_id    = data['product_id'],
                 user_id       = user_id
-
-                ) 
-
+                )
+            return JsonResponse({'Message' : 'SUCCESS_CREATE'}, status = 200)
         except KeyError:
             return JsonResponse({'Message' : 'KEY_ERROR'}, status = 400)
-
-        return JsonResponse({'Message' : 'SUCCESS_CREATE'}, status = 200)
